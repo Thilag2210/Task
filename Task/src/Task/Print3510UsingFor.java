3039fedeff31c18c078a5409c9283f881654cc4a
Thilag2210 <138859070+Thilag2210@users.noreply.github.com>
2023-07-14 11:32:25 +0530

    Add files via upload

---
+++ Task/src/Task/Print3510UsingFor.java
@@ -0,0 +1,13 @@
+package Task;
+
+public class Print3510UsingFor {
+
+	public static void main(String[] args) {
+		int a=10;
+		for (int i=3;i>0;i--) {
+			System.out.println(a/i);
+		}
+
+	}
+
+}
---
+++ Task/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Thilagaraj
+ *
+ */
+module Task {
+}
